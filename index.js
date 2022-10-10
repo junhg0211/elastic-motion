cbc9607cf8839329686d60fac7d02fdb3c5318ef
Sch <junhg0211@gmail.com>
2022-10-10 22:26:48 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+class Square {
+    static DT = 30; // distance threshold
+
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.x2 = x + width;
+        this.y2 = y + height;
+
+        this.tx = x;
+        this.ty = y;
+        this.tx2 = x + width;
+        this.ty2 = y + height;
+
+        this.vx = 0;
+        this.vy = 0;
+        this.vx2 = 0;
+        this.vy2 = 0;
+
+        this.sc = 0; // selected corner
+    }
+
+    applyMovement() {
+        let dx = this.tx - this.x;
+        let dy = this.ty - this.y;
+        let dx2 = this.tx2 - this.x2;
+        let dy2 = this.ty2 - this.y2;
+
+        let gelatiness = 0.1;
+        let friction = 0.2;
+
+        this.vx += gelatiness * dx - friction * this.vx;
+        this.vy += gelatiness * dy - friction * this.vy;
+        this.vx2 += gelatiness * dx2 - friction * this.vx2;
+        this.vy2 += gelatiness * dy2 - friction * this.vy2;
+    }
+
+    resize() {
+        if (!this.sc) {
+            return;
+        }
+
+        let dx = mouseX - pmouseX;
+        let dy = mouseY - pmouseY;
+
+        if (this.sc === 5) {
+            this.tx += dx;
+            this.tx2 += dx;
+            this.ty += dy;
+            this.ty2 += dy;
+            return;
+        }
+
+        if (this.sc === 2 || this.sc === 3) this.tx += dx;
+        else this.tx2 += dx;
+
+        if (this.sc === 1 || this.sc === 2) this.ty += dy;
+        else this.ty2 += dy;
+    }
+
+    draw() {
+        this.resize();
+
+        this.applyMovement();
+
+        this.x += this.vx;
+        this.y += this.vy;
+        this.x2 += this.vx2;
+        this.y2 += this.vy2;
+
+        noStroke();
+        fill(0);
+        rect(this.x, this.y, this.x2 - this.x, this.y2 - this.y, 20);
+
+        textAlign(CENTER, CENTER);
+        textSize(32);
+        textFont('Pretendard, "Noto Sans"')
+        fill(255);
+        text('Shtelo', (this.x + this.x2)/2, (this.y + this.y2)/2)
+    }
+
+    selectCorner() {
+        if (Math.hypot(mouseX - this.x2, mouseY - this.y) < Square.DT) return 1;
+        if (Math.hypot(mouseX - this.x, mouseY - this.y) < Square.DT) return 2;
+        if (Math.hypot(mouseX - this.x, mouseY - this.y2) < Square.DT) return 3;
+        if (Math.hypot(mouseX - this.x2, mouseY - this.y2) < Square.DT) return 4;
+        if (
+            this.x < mouseX && mouseX < this.x2
+            && this.y < mouseY && mouseY < this.y2
+        ) return 5;
+
+        return 0;
+    }
+
+    mousePressed() {
+        this.sc = this.selectCorner();
+    }
+
+    mouseReleased() {
+        this.sc = 0;
+    }
+}
+
+let square;
+
+function mousePressed() {
+    square.mousePressed();
+}
+
+function mouseReleased() {
+    square.mouseReleased();
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    square = new Square(100, 100, 100, 100);
+}
+
+function draw() {
+    background(255);
+
+    square.draw();
+}
